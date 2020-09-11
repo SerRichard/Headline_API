0ab20ed95bb501cd9db320b8c4d5d3ab866c700c
Sean <63349506+SerRichard@users.noreply.github.com>
2020-09-11 15:19:16 +0100

    Add files via upload

--- sky_api.py
+++ sky_api.py
@@ -1,30 +1,75 @@
+# Daily news API
+# Author Sean Hoyal, 2020
+# Ver 1.0
+#
+# Implements jobs from sky_scraping Ver 2.0
+#
+# Imports
 import os
-import json
-import requests
 from flask import Flask, request, jsonify, abort, url_for
+from apscheduler.schedulers.background import BackgroundScheduler
 from cassandra.cluster import Cluster
+from sky_scraping import Scraper
+from datetime import date
+import requests
+import schedule
+import json
+import time
 
 # Initialize cluster connection
-cluster = Cluster(contact_points=['0.0.0.0'],port=9042)
+cluster = Cluster(contact_points=['172.17.0.2'],port=9042)
 session = cluster.connect()
 
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
-# Extensions
-# ...
+# Instantiate Scraper & Schedule jobs
+scrap1 = Scraper
 
+sched = BackgroundScheduler(daemon=True)
+sched.add_job(scrap1.reset,'interval',hours=23,minutes=59)
+sched.add_job(scrap1.scrape,'interval',hours=23,minutes=59,seconds=5)
+sched.add_job(scrap1.addBinary,'interval',hours=23,minutes=59,seconds=10)
+sched.add_job(scrap1.migrateCass,'interval',hours=23,minutes=59,seconds=15)
+sched.start()
+
+# API Routes
 @app.route('/')
 def hello():
-    return('<h1> Hello! Welcome to my scrapes web API, currently scaping from Sky! Visit /articles for all current articles<h1>')
+    return('<h1> Hello! Welcome to my scrapes web API, currently scaping from Sky! Visit /articles for daily current articles<h1>')
 
 @app.route('/articles', methods=['GET'])
-def articles():
-    tuples = session.execute("""Select * From c02.stats""")
+def all():
+    tuples = session.execute("""Select * From news.daily""")
     results = []
     for x in tuples:
-        results.append({})
+        results.append({"title":x.title,"date":x.date})
     return jsonify(results), 200
 
+@app.route('/today', methods=['GET'])
+def current():
+    tuples = session.execute("""Select * From news.daily WHERE date='{}' ALLOW FILTERING""".format(str(date.today())))
+    results = []
+    for x in tuples:
+        results.append({"title":x.title,"date":x.date})
+    return jsonify(results), 200
+
+@app.route('/brexit', methods=['GET'])
+def brexit():
+    tuples = session.execute("""Select * From news.daily WHERE brexit=1 ALLOW FILTERING""")
+    results = []
+    for x in tuples:
+        results.append({"title":x.title,"date":x.date})
+    return jsonify(results), 200
+
+@app.route('/covid', methods=['GET'])
+def covid():
+    tuples = session.execute("""Select * From news.daily WHERE covid=1 ALLOW FILTERING""")
+    results = []
+    for x in tuples:
+        results.append({"title":x.title,"date":x.date})
+    return jsonify(results), 200
+
+# App run
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
--- sky_scraping.py
+++ sky_scraping.py
@@ -1,67 +1,166 @@
 # Sky News daily web scraper
 # Author Sean Hoyal, 2020
 # Ver 2.0
-#
-# Get the headlines in time for coffee!
+# This version carries out the same scrape
+# Additional methods:
+# addBinary() to add binary indicators for articles about Brexit or Covid
+# reset() to reset all files in directory
 #
 # Imports
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
 from datetime import datetime
+from csv import DictReader, DictWriter, reader, writer
+from itertools import islice
+import pandas as pd
+import subprocess
+import uuid
+import csv
 
-# Function to scrape
-def scrape():
-	my_url = 'https://news.sky.com/'
+class Scraper:
+	@classmethod
+	def scrape(self):
+		my_url = 'https://news.sky.com/'
 
-	#Opens connection, downloads page, then closes connection
-	uReqSky = uReq(my_url)
-	sky_html = uReqSky.read()
-	uReqSky.close()
+		#Opens connection, downloads page, then closes connection
+		uReqSky = uReq(my_url)
+		sky_html = uReqSky.read()
+		uReqSky.close()
 
-	# Parse into beautiful soup, place all of a particular class into containers
-	news_soup = soup(sky_html, "html.parser")
+		# Parse into beautiful soup, place all of a particular class into containers
+		news_soup = soup(sky_html, "html.parser")
 
-	containers = news_soup.findAll("div",{"class":"sdc-site-tile__body"})
-	trendings = news_soup.findAll("li",{"class":"sdc-site-trending__item"})
+		containers = news_soup.findAll("div",{"class":"sdc-site-tile__body"})
+		trendings = news_soup.findAll("li",{"class":"sdc-site-trending__item"})
 
-	# Prepare csv to record data
-	filename = "sky_daily_articles.csv"
-	f = open(filename, "a")
-	headers = "title, date, trending\n"
-	f.write(headers)
+		# Prepare csv to record data
+		filename = "scraped_articles.csv"
+		f = open(filename, "a+")
 
-	# Loop for non-trending articles: Sky
-	for container in containers:
-		try:
-			article_titles = container.findAll("span",{"class":"sdc-site-tile__headline-text"})
-			title = article_titles[0].text
-		except IndexError:
-			title = "N/A"
+		# Loop for non-trending articles: Sky
+		for container in containers:
+			uid = uuid.uuid4().hex
+			try:
+				article_titles = container.findAll("span",{"class":"sdc-site-tile__headline-text"})
+				title = article_titles[0].text
+			except IndexError:
+				title = "N/A"
+			
+			date = datetime.today().strftime('%Y-%m-%d')
+			trending = 'No'
+
+			# Write findings to csv
+			f.write("\n" + uid + "," + title.replace(",", "|") + ","  +  date + "," + trending)
+
+		# Loop for the trending articles: Sky
+		for trend in trendings:
+			uid = uuid.uuid4().hex
+			trending_titles = trend.findAll("a",{"class":"sdc-site-trending__link"})
+			title = trending_titles[0].text
+
+			date = datetime.today().strftime('%Y-%m-%d')
+			trending = 'Yes'
+
+			# Write findings to csv
+			f.write("\n" + uid + "," + title.replace(",", "|") + ","  +  date + "," + trending)
+
+		print("Scrape complete")
+		f.close()
+
+	@classmethod
+	def addBinary(self):
 		
-		date = datetime.today().strftime('%Y-%m-%d')
-		trending = 'No'
+		# Find all titles referring to Brexit
+		input_file = "scraped_articles.csv"
+		temp = "temp.csv"
+		output_file = "modified_articles.csv"
 
-		print("Article title: " + title)
-		print("Date published: " + date)
-		print("Trending: " + trending)
+		with open(input_file, "r") as f, \
+			open(temp, 'w', newline='') as g:
 
-	# Write findings to csv
-		f.write(title.replace(",", "|") + ","  +  date + "," + trending + "\n")
+			headers = ['uuid','title','date','trending','brexit']
 
-	# Loop for the trending articles: Sky
-	for trend in trendings:
-		trending_titles = trend.findAll("a",{"class":"sdc-site-trending__link"})
-		title = trending_titles[0].text
+			d_read = DictReader(f)
+			d_write = DictWriter(g, fieldnames=headers)
+		
+			d_write.writeheader()
 
-		date = datetime.today().strftime('%Y-%m-%d')
-		trending = 'Yes'
+			brex = ("brexit", "BREXIT", "Brexit")
 
-		print("Article title: " +  title)
-		print("Date published: " + date)
-		print("Trending: " + trending)
+			for row in d_read:
+				if any(x in row["title"] for x in brex):
+					row["brexit"] = 1
+					d_write.writerow(row)
+				else:
+					row["brexit"] = 0
+					d_write.writerow(row)
+			
+		f.close()
+		g.close()
 
-	# Write findings to csv
-		f.write(title.replace(",", "|") + ","  +  date + "," + trending + "\n")
+		with open(temp, "r") as f, \
+			open(output_file, 'w', newline='') as g:
 
-	f.close()
+			cov = ("coronavirus", "Coronavirus", "CORONAVIRUS", "COVID", "Covid", "covid")
 
+			headers = ['uuid','title','date','trending','brexit','covid']
+
+			d_read = DictReader(f)
+			d_write = DictWriter(g, fieldnames=headers)
+		
+			d_write.writeheader()
+
+			for row in d_read:
+				if any(x in row["title"] for x in cov):
+					row["covid"] = 1
+					d_write.writerow(row)
+				else:
+					row["covid"] = 0
+					d_write.writerow(row)
+
+		print("Modification complete")
+		f.close()
+		g.close()
+
+	@classmethod
+	def reset(self):
+		# Open csv and truncate
+		filename = "temp.csv"
+		f = open(filename, "w+")
+		f.close()
+
+		# Open csv and truncate
+		filename = "scraped_articles.csv"
+		f = open(filename, "w+")
+		f.close()
+
+		# Open csv and prepare headers
+		filename = "scraped_articles.csv"
+		f = open(filename, "a+")
+		f.write('uuid,title,date,trending')
+		f.close()
+
+		# Open csv and truncate
+		filename = "modified_articles.csv"
+		f = open(filename, "w+")
+		f.close()
+
+		# Open csv and prepare headers
+		filename = "modified_articles.csv"
+		f = open(filename, "a+")
+		f.write('uuid,title,date,trending,brexit,covid')
+		f.close()
+
+		print("Files reset")
+
+	@classmethod
+	def migrateCass(self):
+		comm1 = "sudo docker cp modified_articles.csv news-dock:/home/modified_articles.csv"
+
+		normal = subprocess.run(comm1,
+			stdout = subprocess.PIPE, stderr = subprocess.PIPE,
+			check = True,
+			text = True,
+			shell = True)
+		print(normal.stdout)
+		print("Complete!")
